3fcfb666abe3312e16cd5da2db713d2387bcd89b
DXVVAY <Arazrstmy500@gmail.com>
2024-01-28 18:41:13 +0100

    fr

--- core/funcs/pron_changer.py
+++ core/funcs/pron_changer.py
@@ -9,7 +9,7 @@ def pchanger(pronouns: str, token: str):
         log.errors(token, result.text, result.status_code)
 
 def pron_changer():
-    set_title("Pronoun Changer")
+    set_title("Pronouns Changer")
     pronouns = utility.ask("Pronouns")
     thread = utility.ask("Thread Count")
     utility.run_threads(max_threads=thread, func=pchanger, args=[pronouns])
